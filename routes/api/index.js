7d1b2b54696ababe4455a22843e7f8bdb91d80a7
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-06-23 20:12:16 -0400

    create backend for heroku deployment

---
+++ routes/api/index.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+// //Spanish Routes
+// router.use("/spanish", spanishRoutes);
+// //French Routes
+// router.use("/french", frenchRoutes);
+// // German Routes
+// router.use("/german", germanRoutes);
+// // Italian routes
+// router.use("/italian", italianRoutes);
+// // Portuguese routes
+// router.use("/portuguese", portugueseRoutes);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+// Define middleware for JSON parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//api routes
+// app.use(routes);
+
+
+// // If no API routes are hit, send the React app
+// app.use(function (req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// Connect to Mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rgLanguage",
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+)
+.then(res => {
+  // console.log("res: ", res);
+  console.log("mongodb is successfully connected ✔");
+})
+.catch(err => console.log("err!: ", err))
+;
+
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
